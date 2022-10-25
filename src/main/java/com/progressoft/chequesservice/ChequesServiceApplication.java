3204109803eed0a927c0aae20f47b8b546e75479
joesakkab <joesakkab@gmail.com>
2022-10-25 10:07:12 +0300

    project spring boot creation with web-app dependency and H2 dependency

---
+++ src/main/java/com/progressoft/chequesservice/ChequesServiceApplication.java
@@ -0,0 +1,13 @@
+package com.progressoft.chequesservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChequesServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChequesServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/progressoft/chequesservice/ChequesServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.progressoft.chequesservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChequesServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
